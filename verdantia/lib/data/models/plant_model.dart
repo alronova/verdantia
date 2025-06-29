ec76052eda53077a89759accdd7e6d256f949e95
avantika <namelyavantika@gmail.com>
2025-06-29 21:57:20 +0530

    refactor: minor changes

--- verdantia/lib/data/models/plant_model.dart
+++ verdantia/lib/data/models/plant_model.dart
@@ -1,21 +1,58 @@
-// import 'dart:ffi';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Plant {
   final String plantId;
   final String uid;
   final String type;
-  final Map<String, String> appearance;
+  final String appearance; // sad, happy, ok
   final int hp;
   final int growthStage;
-  final Map<String, String> lastCare;
+  final Map<String, Timestamp> lastCare;
 
-  const Plant({
+  Plant({
     required this.plantId,
     required this.uid,
     required this.type,
-    required this.appearance,
+    this.appearance = "ok",
     required this.hp,
     required this.growthStage,
     required this.lastCare,
   });
+
+  factory Plant.fromJson(Map<String, dynamic> json) => Plant(
+        plantId: json['plantId'],
+        uid: json['uid'],
+        type: json['type'],
+        appearance: json['appearance'],
+        hp: json['hp'],
+        growthStage: json['growthStage'],
+        lastCare: Map<String, Timestamp>.from(json['lastCare']),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'plantId': plantId,
+        'uid': uid,
+        'type': type,
+        'appearance': appearance,
+        'hp': hp,
+        'growthStage': growthStage,
+        'lastCare': lastCare,
+      };
+
+  Plant copyWith({
+    int? hp,
+    int? growthStage,
+    String? appearance,
+    Map<String, Timestamp>? lastCare,
+  }) {
+    return Plant(
+      plantId: plantId,
+      uid: uid,
+      type: type,
+      appearance: appearance ?? this.appearance,
+      hp: hp ?? this.hp,
+      growthStage: growthStage ?? this.growthStage,
+      lastCare: lastCare ?? this.lastCare,
+    );
+  }
 }
